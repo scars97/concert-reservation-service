75e212ff07b66d77779c1f2298a16216f94329e0
박성현 <qkr970504@gmail.com>
2024-12-29 23:17:30 +0900

    build project

---
+++ src/main/java/com/hhconcert/server/ConcertReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.hhconcert.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcertReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConcertReservationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hhconcert/server/ConcertReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhconcert.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcertReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
