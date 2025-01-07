c006ac0ed3c5bae1e079653c672b8bf193cdc5d7
박성현 <qkr970504@gmail.com>
2025-01-07 19:44:01 +0900

    [feat] 콘서트 스케줄 infra 구현

---
+++ src/main/java/com/hhconcert/server/infrastructure/schedule/ScheduleJpaRepository.java
@@ -0,0 +1,11 @@
+package com.hhconcert.server.infrastructure.schedule;
+
+import com.hhconcert.server.business.domain.schedule.entity.Schedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ScheduleJpaRepository extends JpaRepository<Schedule, Long> {
+
+    List<Schedule> findByConcertId(Long concertId);
+}
