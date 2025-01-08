7c01f198b9b1f2dd202747117e45a0d63c76d289
박성현 <qkr970504@gmail.com>
2025-01-09 01:54:25 +0900

    [feat] 포인트 충전/사용 구현

--- src/main/java/com/hhconcert/server/business/domain/user/service/UserService.java
+++ src/main/java/com/hhconcert/server/business/domain/user/service/UserService.java
@@ -1,7 +1,11 @@
 package com.hhconcert.server.business.domain.user.service;
 
+import com.hhconcert.server.business.domain.user.dto.PointInfo;
+import com.hhconcert.server.business.domain.user.dto.PointResult;
 import com.hhconcert.server.business.domain.user.dto.UserResult;
+import com.hhconcert.server.business.domain.user.entity.User;
 import com.hhconcert.server.business.domain.user.persistance.UserRepository;
+import com.hhconcert.server.global.exception.PointException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,4 +21,25 @@ public class UserService {
         return UserResult.from(userRepository.findUser(userId));
     }
 
+    @Transactional
+    public PointResult chargePoint(PointInfo info) {
+        User user = userRepository.findUser(info.userId());
+
+        user.chargePoint(info.amount());
+
+        return PointResult.from(user);
+    }
+
+    @Transactional
+    public PointResult usePoint(PointInfo info) {
+        User user = userRepository.findUser(info.userId());
+
+        if (user.getPoint() < info.amount()) {
+            throw new PointException("잔액이 부족합니다.");
+        }
+
+        user.usePoint(info.amount());
+
+        return PointResult.from(user);
+    }
 }
