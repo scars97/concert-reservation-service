9a887fc1e10d6f56991ad2dde80ec12d0ee9844a
박성현 <qkr970504@gmail.com>
2025-01-10 05:47:21 +0900

    [feat] 포인트 UseCase 구현

--- src/main/java/com/hhconcert/server/interfaces/api/point/dto/PointRequest.java
+++ src/main/java/com/hhconcert/server/interfaces/api/point/dto/PointRequest.java
@@ -1,7 +1,12 @@
 package com.hhconcert.server.interfaces.api.point.dto;
 
+import com.hhconcert.server.business.domain.user.dto.PointInfo;
+
 public record PointRequest (
         String userId,
-        Integer amount
+        int amount
 ){
+    public PointInfo toInfo() {
+        return new PointInfo(userId, amount);
+    }
 }
