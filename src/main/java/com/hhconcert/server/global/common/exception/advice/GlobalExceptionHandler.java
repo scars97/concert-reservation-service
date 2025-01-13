deb2879b10ba5824d8a5fa3a589566cfa0a9c679
박성현 <qkr970504@gmail.com>
2025-01-13 23:51:55 +0900

    [feat] 사용자 입력 validation 예외 처리

--- src/main/java/com/hhconcert/server/global/common/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/hhconcert/server/global/common/exception/advice/GlobalExceptionHandler.java
@@ -3,18 +3,34 @@ package com.hhconcert.server.global.common.exception.advice;
 import com.hhconcert.server.global.common.exception.definitions.PaymentException;
 import com.hhconcert.server.global.common.exception.definitions.PointException;
 import com.hhconcert.server.global.common.exception.definitions.TokenException;
+import com.hhconcert.server.global.common.model.BindErrorResponse;
 import com.hhconcert.server.global.common.model.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<BindErrorResponse> validationExceptionHandler(BindException exception) {
+        Map<String, String> errors = new HashMap<>();
+        exception.getFieldErrors().forEach(error -> {
+            String fieldName = error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+            log.error("Invalid Input : {} - {}", fieldName, errorMessage);
+        });
+        return BindErrorResponse.of(HttpStatus.BAD_REQUEST, errors);
+    }
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
