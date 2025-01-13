4b27a683762a37f192c442dc210d4828ea3dcb6c
박성현 <qkr970504@gmail.com>
2025-01-13 23:11:48 +0900

    [feat] 글로벌 핸들러 생성 및 에러 로그 적용

--- src/main/java/com/hhconcert/server/global/common/exception/advice/GlobalExceptionHandler.java
+++ src/main/java/com/hhconcert/server/global/common/exception/advice/GlobalExceptionHandler.java
@@ -1,10 +1,10 @@
 package com.hhconcert.server.global.common.exception.advice;
 
+import com.hhconcert.server.global.common.exception.definitions.PaymentException;
+import com.hhconcert.server.global.common.exception.definitions.PointException;
+import com.hhconcert.server.global.common.exception.definitions.TokenException;
 import com.hhconcert.server.global.common.model.ErrorResponse;
-import com.hhconcert.server.global.common.exception.ConcertException;
-import com.hhconcert.server.global.common.exception.PaymentException;
-import com.hhconcert.server.global.common.exception.TokenException;
-import com.hhconcert.server.global.common.exception.UnAuthorizationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,36 +12,31 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
 
+@Slf4j
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
     public ResponseEntity<ErrorResponse> handleNoSuchElementException(NoSuchElementException e) {
         return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(HttpStatus.NOT_FOUND.toString(), e.getMessage()));
-    }
-
-    @ExceptionHandler(ConcertException.class)
-    public ResponseEntity<ErrorResponse> handleConcertException(ConcertException ex) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(new ErrorResponse(HttpStatus.NOT_FOUND.toString(), ex.getMessage()));
+                .body(new ErrorResponse(HttpStatus.NOT_FOUND, e.getMessage()));
     }
 
     @ExceptionHandler(PaymentException.class)
     public ResponseEntity<ErrorResponse> handlePaymentException(PaymentException ex) {
-        return ResponseEntity.status(HttpStatus.PAYMENT_REQUIRED)
-                .body(new ErrorResponse(HttpStatus.PAYMENT_REQUIRED.toString(), ex.getMessage()));
+        log.error("Payment Exception occurred : {}", ex.getErrorCode());
+        return ErrorResponse.of(ex.getErrorCode());
     }
 
-    @ExceptionHandler(UnAuthorizationException.class)
-    public ResponseEntity<ErrorResponse> handleUnAuthorizationException(UnAuthorizationException ex) {
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ErrorResponse(HttpStatus.UNAUTHORIZED.toString(), ex.getMessage()));
+    @ExceptionHandler(PointException.class)
+    public ResponseEntity<ErrorResponse> handlePointException(PointException ex) {
+        log.error("Point Exception occurred : {}", ex.getErrorCode());
+        return ErrorResponse.of(ex.getErrorCode());
     }
 
     @ExceptionHandler(TokenException.class)
     public ResponseEntity<ErrorResponse> handleTokenException(TokenException ex) {
-        return ResponseEntity.status(HttpStatus.FORBIDDEN)
-                .body(new ErrorResponse(HttpStatus.FORBIDDEN.toString(), ex.getMessage()));
+        log.error("Token Exception occurred : {}", ex.getErrorCode());
+        return ErrorResponse.of(ex.getErrorCode());
     }
 }
