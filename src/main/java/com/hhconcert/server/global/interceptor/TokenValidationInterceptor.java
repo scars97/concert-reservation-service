39a342d75db8cac9a592aa1b380f23a7388f8c2e
박성현 <qkr970504@gmail.com>
2025-01-14 23:18:18 +0900

    [refactor] interceptor 불필요한 로그 제거

--- src/main/java/com/hhconcert/server/global/interceptor/TokenValidationInterceptor.java
+++ src/main/java/com/hhconcert/server/global/interceptor/TokenValidationInterceptor.java
@@ -22,24 +22,16 @@ public class TokenValidationInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         String token = request.getHeader("Authorization");
 
-        log.info("Received request: URI={}, Method={}, Token={}",
-                request.getRequestURI(),
-                request.getMethod(),
-                token != null ? token : "No Token");
-
         if (token == null) {
-            log.warn("Authorization header is missing.");
             return errorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, "토큰 정보가 누락되었습니다.");
         }
 
         if (!StringUtils.hasText(token) || !token.startsWith("Bearer ")) {
-            log.warn("Invalid token format: {}", token);
             return errorResponse(response, HttpServletResponse.SC_FORBIDDEN, "잘못된 토큰입니다.");
         }
 
         try {
             String extractedToken = token.substring(7);
-            log.debug("Extracted Token: {}", extractedToken);
             tokenRepository.findToken(extractedToken);
         } catch (RuntimeException e) {
             log.error("Token not found: {}", token, e);
