a0bb278d294095166b0f764c660b972e91bbd61f
박성현 <qkr970504@gmail.com>
2025-01-30 22:20:25 +0900

    [chore] 대기열 Swagger 문서 정리

--- src/main/java/com/hhconcert/server/interfaces/api/queues/dto/TokenRequest.java
+++ src/main/java/com/hhconcert/server/interfaces/api/queues/dto/TokenRequest.java
@@ -1,9 +1,11 @@
 package com.hhconcert.server.interfaces.api.queues.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record TokenRequest (
         @NotNull(message = "사용자 ID는 필수 입력 값입니다.")
+        @Schema(description = "사용자 ID",  example = "user1234")
         String userId
 ) {
 }
