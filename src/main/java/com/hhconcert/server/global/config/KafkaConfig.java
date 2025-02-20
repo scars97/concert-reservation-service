2aea2f28dbc6747d14ccabd61524331a07c22f57
박성현 <qkr970504@gmail.com>
2025-02-21 01:22:42 +0900

    [test] kafka 연동 통합 테스트

---
+++ src/main/java/com/hhconcert/server/global/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package com.hhconcert.server.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+        factory.setConcurrency(1); // 리스너 쓰레드 개수
+        factory.getContainerProperties().setPollTimeout(3000);
+        return factory;
+    }
+}
